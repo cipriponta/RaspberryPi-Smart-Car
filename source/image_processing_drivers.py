9495d569602272b042fea54cba50507085cd8614
Ciprian Ponta <ponta_ciprian@yahoo.com>
2023-02-11 18:05:34 +0200

    [8] Solved camera issues

--- source/image_processing_drivers.py
+++ source/image_processing_drivers.py
@@ -3,9 +3,14 @@ from picamera import PiCamera
 import time
 import cv2
 
+CAMERA_RESOLUTION = (640, 480)
+CAMERA_FRAMERATE = 10
+
 class ImageProcessor:
     def __init__(self):
         self.camera = PiCamera()
+        self.camera.resolution = CAMERA_RESOLUTION
+        self.camera.framerate = CAMERA_FRAMERATE
         self.capture = PiRGBArray(self.camera)
         time.sleep(0.1)
 
