77f1e7b686a4ee6dd2196c8dc8665cac4e95d6b2
Ciprian Ponta <ponta_ciprian@yahoo.com>
2023-03-26 21:48:10 +0300

    [17] Created a basic lane detection algorithm

--- source/drivers/image_processing_drivers.py
+++ source/drivers/image_processing_drivers.py
@@ -33,14 +33,30 @@ class ImageProcessor:
         # Clear the stream
         self.raw_capture.truncate(0)
 
-        processed_frame = self.get_lines(frame)
+        greyscale_frame, blurred_frame, thresholded_frame, contours = self.get_contours(frame)
+        rectangle_outlines, lines = self.get_lines(contours)
+
+        output_frame = None
+        if IMAGE_OUTPUT_FRAME == IMAGE_OUTPUT.COLOR:
+            output_frame = frame
+        elif IMAGE_OUTPUT_FRAME == IMAGE_OUTPUT.GREYSCALE:
+            output_frame = cv2.cvtColor(greyscale_frame, cv2.COLOR_GRAY2BGR)
+        elif IMAGE_OUTPUT_FRAME == IMAGE_OUTPUT.BLURRED:
+            output_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_GRAY2BGR)
+        elif IMAGE_OUTPUT_FRAME == IMAGE_OUTPUT.THRESHOLDED:
+            output_frame = cv2.cvtColor(thresholded_frame, cv2.COLOR_GRAY2BGR)
+        
+        processed_frame = self.draw_output(output_frame = output_frame,
+                                            contours = contours,
+                                            rectangle_outlines = rectangle_outlines,
+                                            lines = lines)
 
         if self.is_debug:
             self.video_streamer.send_frame(processed_frame)
 
         return processed_frame
 
-    def get_lines(self, frame):
+    def get_contours(self, frame):
         greyscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         blurred_frame = IMAGE_THRESHOLD_BLUR_METHOD(src =  greyscale_frame,
                                                     ksize = (IMAGE_THRESHOLD_BLOCK_SIZE, IMAGE_THRESHOLD_BLOCK_SIZE),
@@ -53,25 +69,57 @@ class ImageProcessor:
                                                   C = IMAGE_THRESHOLD_SUBTRACTED_CONSTANT)
         contours, hierarchy = cv2.findContours(image = thresholded_frame,
                                                mode = IMAGE_CONTOUR_RETRIEVAL_MODE,
-                                               method = IMAGE_CONTOUR_APPROX_METHOD)             
-
-        output_frame = cv2.cvtColor(thresholded_frame, cv2.COLOR_GRAY2BGR)
-        cv2.drawContours(image = output_frame,
-                         contours = contours,
-                         contourIdx = -1, 
-                         color = IMAGE_LINES_COLOR,
-                         thickness = IMAGE_LINES_THICKNESS)
+                                               method = IMAGE_CONTOUR_APPROX_METHOD)
         
-        rectangle_list = []
+        return greyscale_frame, blurred_frame, thresholded_frame, contours
+
+    def get_lines(self, contours):
+        lines_list = []
+        rectangle_outlines = []
+
         for contour in contours:
             rectangle = cv2.minAreaRect(contour)
             rectangle_outline = numpy.int0(cv2.boxPoints(rectangle))
-            print(rectangle_outline)
-            try:
-                cv2.drawContours(output_frame, [rectangle_outline], 0, (0, 0, 255), 1)
-            except Exception as e:
-                print(e)
-        
+            rectangle_outlines.append(rectangle_outline)
+            
+            middle1 = numpy.array([int((rectangle_outline[0][0] + rectangle_outline[1][0]) / 2), 
+                                   int((rectangle_outline[0][1] + rectangle_outline[1][1]) / 2)])
+            middle2 = numpy.array([int((rectangle_outline[1][0] + rectangle_outline[2][0]) / 2), 
+                                   int((rectangle_outline[1][1] + rectangle_outline[2][1]) / 2)])
+            middle3 = numpy.array([int((rectangle_outline[2][0] + rectangle_outline[3][0]) / 2), 
+                                   int((rectangle_outline[2][1] + rectangle_outline[3][1]) / 2)])
+            middle4 = numpy.array([int((rectangle_outline[3][0] + rectangle_outline[0][0]) / 2), 
+                                   int((rectangle_outline[3][1] + rectangle_outline[0][1]) / 2)])
+            
+            if numpy.linalg.norm(middle1 - middle3) > numpy.linalg.norm(middle2 - middle4):
+                lines_list.append([middle1, middle3])
+            else:
+                lines_list.append([middle2, middle4])
+           
+        return rectangle_outlines, lines_list
+    
+    def draw_output(self, output_frame, contours, rectangle_outlines, lines):
+        if IMAGE_DISPLAY_CONTOURS:
+            cv2.drawContours(image = output_frame, 
+                            contours = contours, 
+                            contourIdx = -1, 
+                            color = IMAGE_CONTOUR_COLOR, 
+                            thickness = IMAGE_LINES_THICKNESS)
+            
+        if IMAGE_DISPLAY_BOUNDED_LINES:
+            cv2.drawContours(image = output_frame, 
+                            contours = rectangle_outlines, 
+                            contourIdx = -1, 
+                            color = IMAGE_RECTANGLE_COLOR, 
+                            thickness = IMAGE_LINES_THICKNESS)
+
+        if IMAGE_DISPLAY_LINES:
+            for line in lines:
+                cv2.line(img = output_frame,
+                        pt1 = line[0],
+                        pt2 = line[1],
+                        color = IMAGE_LINES_COLOR,
+                        thickness = IMAGE_LINES_THICKNESS)
         return output_frame
 
     def close(self):
